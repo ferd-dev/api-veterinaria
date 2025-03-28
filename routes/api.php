5b8136a1fd789c44f3da7cbdb4c2660f86cfe095
fernando <vluisfernando62@gmail.com>
2025-03-28 11:42:11 -0400

    fix: habilitar middleware de autenticación para las rutas de usuario en el controlador de autenticación

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'auth',
     // 'middleware' => ['auth:api', 'role:writer'],
-    'middleware' => ['auth:api', 'permission:edit articles'],
+    // 'middleware' => ['auth:api', 'permission:edit articles'],
 ], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/me', [AuthController::class, 'me'])->name('me'); // ->middleware('auth:api')
-    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh'); // ->middleware('auth:api')
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // ->middleware('auth:api')
+    Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
 });
